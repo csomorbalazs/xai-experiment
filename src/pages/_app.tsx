231c5ab1e6151b7b357c7f81f1bb0952401a861c
Balázs Csomor <csomorbp@gmail.com>
2023-02-20 17:34:33 +0100

    Remove clutter, add emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { css, Global } from "@emotion/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            font-family: "Inter", sans-serif;
+            color: #201e1e;
+            margin: 0;
+            padding: 0;
+            font-size: 17px;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"anonymous"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="robots" content="noindex" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
