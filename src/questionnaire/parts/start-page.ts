6b1dbdd262096fe7fbb0d6c560005f4697c63fc1
Balázs Csomor <csomorbp@gmail.com>
2023-03-21 15:35:38 +0100

    Update start page

--- src/questionnaire/parts/start-page.ts
+++ src/questionnaire/parts/start-page.ts
@@ -8,7 +8,7 @@ const startPage = {
           <p>Welcome to the <b>"XAI Evaluation"</b> Experiment!</p>
           </br>
           <p>
-          The experiment is conducted by the <b>Technical University of Berlin at the Quality and Usability Lab</b>. The goal of the study is to gain knowledge about the usefulness of explainability approaches for the use case of fake news detection. Your task will be answering several questions and rate the <b>usefulness of different explanations</b> given by an <b>Artificial Intelligence</b> system for detecting fake news.
+          The experiment is conducted by the <b>Technical University of Berlin at the Quality and Usability Lab</b>. The goal of the study is to gain knowledge about the usefulness of explainability approaches for the use case of fake news detection. Your task will be answering several questions and rate the <b>usefulness of different explanations</b> given by an <b>Artificial Intelligence-System</b> for detecting <b>fake news</b>.
           </p>
           </br>
           <p>
@@ -19,7 +19,7 @@ const startPage = {
           </p>
           </br>
           <p>
-          <b>Privacy Statement:</b> Your responses are collected in an anonymized form only. No backtracking to your person and the given responses is possible. You will be asked to create your personal code in the beginning based on several questions. In case you want to delete your responses you can request deleting within one month after participating in the experiment, by sending your personal code. The data gathered in this experiment will be used for scientific publications. In case you have any questions please contact: <a href="mailto:vera.schmitt@tu-berlin.de">vera.schmitt@tu-berlin.de</a>
+          <b>Privacy Statement:</b> Your responses are collected in an anonymized form only. No backtracking to your person and the given responses is possible. You will be asked to create your personal code in the beginning based on several questions. In case you want to delete your responses you can request deleting within one month after participating in the experiment, by sending your personal code. The data gathered in this experiment will be used for scientific publications. In case you have any questions please contact: <a href="mailto:vera.schmitt@tu-berlin.de">vera.schmitt@tu-berlin.de</a>.
           </p>
           </br>
         </div>
