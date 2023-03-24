e25e46d61a8f7655043783bffd7b6388c2f46424
Balázs Csomor <csomorbp@gmail.com>
2023-03-24 21:06:51 +0100

    Save random values to local storage

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -1,18 +1,35 @@
 import XAIQuestionnaire from "@/components/XAIQuestionnaire";
 import getRandomizedNewsItems from "@/helper/get-randomized-news-items";
+import { XAIFeatureLevel } from "@/model/xai-feature-level";
 
 const Home = () => {
-  // generate random group number (1 or 2)
-  const groupNumber = Math.random() < 0.5 ? 1 : 2;
-  const newsItems = getRandomizedNewsItems(groupNumber);
+  // see if group number and feature are stored in local storage
+  let groupNumber = parseInt(
+    localStorage.getItem("xai-experiment.group") ?? "0"
+  );
+  let feature = localStorage.getItem(
+    "xai-experiment.feature"
+  ) as XAIFeatureLevel | null;
 
-  // get randomized feature (either "salient" or "explanations")
-  const features = Math.random() < 0.5 ? "salient" : "explanations";
+  // if they are not, generate random group number and feature
+  if (groupNumber === 0 || feature === null) {
+    // generate random group number (1 or 2)
+    groupNumber = Math.random() < 0.5 ? 1 : 2;
+
+    // get randomized feature ("salient" or "explanations")
+    feature = Math.random() < 0.5 ? "salient" : "explanations";
+
+    // store group number and feature in local storage
+    localStorage.setItem("xai-experiment.group", groupNumber.toString());
+    localStorage.setItem("xai-experiment.feature", feature);
+  }
+
+  const newsItems = getRandomizedNewsItems(groupNumber);
 
   return (
     <XAIQuestionnaire
       newsItems={newsItems}
-      xaiFeature={features}
+      xaiFeature={feature}
       groupNumber={groupNumber}
     />
   );
